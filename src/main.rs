586362f099f3fee10f802f05a5f1e12720dbe101
Luna <luna.dragon@suse.com>
2023-10-04 11:22:54 +0530

    de detection

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,27 @@
-use anyhow::{Context, Result};
-use std::{env::consts::ARCH, process::Command};
-use walkdir::{DirEntry, WalkDir};
+use anyhow::{anyhow, Context, Result};
+use std::env::{consts::ARCH, var};
 
 use hostnamectl::Host;
-use utils::{ps::terminal, which::which};
+use package_managers::get_package_number;
+use utils::ps::terminal;
 mod hostnamectl;
+mod package_managers;
 mod utils;
 
-fn is_dir(entry: &DirEntry) -> bool {
-    entry.path().is_dir()
-}
-
-fn get_package_number() -> Result<String> {
-    let mut rpm_number = 0;
-    let mut flatpak_number = 0;
-    let mut emerge_number = 0;
-    let mut out = String::from("(");
-    if which("rpm").is_some() {
-        let rpm_out = Command::new("rpm").arg("-qa").output()?;
-        let output = String::from_utf8_lossy(&rpm_out.stdout).to_string();
-        rpm_number = output.lines().count();
-        if rpm_number > 0 {
-            if out == "(" {
-                out.push_str("rpm");
-            } else {
-                out.push_str(",rpm");
-            }
+pub fn desktop_env() -> Result<String> {
+    let desktop = match var("XDG_CURRENT_DESKTOP") {
+        Ok(de) => de,
+        Err(e) => {
+            return Err(anyhow!(
+                "{e}\nXDG_CURRENT_DESKTOP not set failed to determain desktop env"
+            ))
         }
+    };
+
+    match desktop.as_ref() {
+        "KDE" => return Ok(String::from("Kde Plasma")),
+        _ => return Err(anyhow!("unkown value {desktop}")),
     }
-
-    if which("emerge").is_some() {
-        let walker = WalkDir::new("/var/db/pkg/").into_iter();
-        for entry in walker.filter_entry(|e| is_dir(e)) {
-            match entry {
-                Err(_) => continue,
-                Ok(_) => emerge_number += 1,
-            }
-        }
-        if emerge_number > 0 {
-            if out == "(" {
-                out.push_str("emerge");
-            } else {
-                out.push_str(",emerge");
-            }
-        }
-    }
-
-    if which("flatpak").is_some() {
-        let flatpak_out = Command::new("flatpak").arg("list").output()?;
-        let output = String::from_utf8_lossy(&flatpak_out.stdout).to_string();
-        flatpak_number = output.lines().count();
-        if flatpak_number > 0 {
-            if out == "(" {
-                out.push_str("flatpak");
-            } else {
-                out.push_str(",flatpak");
-            }
-        }
-    }
-
-    out.push(')');
-
-    Ok(format!(
-        "{}: {}",
-        out,
-        emerge_number + rpm_number + flatpak_number
-    ))
 }
 
 fn main() -> Result<()> {
@@ -78,8 +34,9 @@ fn main() -> Result<()> {
     let hardware_model = host.clone().hardware_model();
     let os = host.operating_system_pretty_name();
     let os_out = format!("{os} {ARCH}");
+    let desktop = desktop_env()?;
 
-    println!("{a}\n{terminal}\n{hostname}\n{kernel_version}\n{hardware_vendor}\n{hardware_model}\n{os_out}");
+    println!("{a}\n{terminal}\n{hostname}\n{kernel_version}\n{hardware_vendor}\n{hardware_model}\n{os_out}\n{desktop}");
 
     Ok(())
 }
---
+++ src/package_managers.rs
@@ -0,0 +1,65 @@
+use super::utils::which::which;
+use anyhow::Result;
+use std::process::Command;
+use walkdir::{DirEntry, WalkDir};
+
+fn is_dir(entry: &DirEntry) -> bool {
+    entry.path().is_dir()
+}
+
+pub fn get_package_number() -> Result<String> {
+    let mut rpm_number = 0;
+    let mut flatpak_number = 0;
+    let mut emerge_number = 0;
+    let mut out = String::from("(");
+    if which("rpm").is_some() {
+        let rpm_out = Command::new("rpm").arg("-qa").output()?;
+        let output = String::from_utf8_lossy(&rpm_out.stdout).to_string();
+        rpm_number = output.lines().count();
+        if rpm_number > 0 {
+            if out == "(" {
+                out.push_str("rpm");
+            } else {
+                out.push_str(",rpm");
+            }
+        }
+    }
+
+    if which("emerge").is_some() {
+        let walker = WalkDir::new("/var/db/pkg/").into_iter();
+        for entry in walker.filter_entry(|e| is_dir(e)) {
+            match entry {
+                Err(_) => continue,
+                Ok(_) => emerge_number += 1,
+            }
+        }
+        if emerge_number > 0 {
+            if out == "(" {
+                out.push_str("emerge");
+            } else {
+                out.push_str(",emerge");
+            }
+        }
+    }
+
+    if which("flatpak").is_some() {
+        let flatpak_out = Command::new("flatpak").arg("list").output()?;
+        let output = String::from_utf8_lossy(&flatpak_out.stdout).to_string();
+        flatpak_number = output.lines().count();
+        if flatpak_number > 0 {
+            if out == "(" {
+                out.push_str("flatpak");
+            } else {
+                out.push_str(",flatpak");
+            }
+        }
+    }
+
+    out.push(')');
+
+    Ok(format!(
+        "{}: {}",
+        out,
+        emerge_number + rpm_number + flatpak_number
+    ))
+}
