6d9b49c417d86a9e897eed5c0a00408c6fdec6a8
Luna <luna.dragon@suse.com>
2023-10-16 18:26:40 +0530

    add nix formatter

--- flake.nix
+++ flake.nix
@@ -12,47 +12,55 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
-     naersk = {
+    naersk = {
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
-     };
+    };
   };
 
-  outputs = { self, nixpkgs, nil, rust-overlay,flake-utils, naersk  }: 
-  flake-utils.lib.eachDefaultSystem (system:
-    let 
+  outputs = {
+    self,
+    nixpkgs,
+    nil,
+    rust-overlay,
+    flake-utils,
+    naersk,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
       overlays = [(import rust-overlay)];
-    pkgs = import nixpkgs {
-      inherit system overlays;
-    }; in with pkgs; {
-    formatter = pkgs.alejandra;
-    devShells.default = mkShell {
-      buildInputs = with pkgs; [
-        pkgs.zsh
-        pkgs.nil
-        rust-bin.beta.latest.default
-        pkgs.rust-analyzer
-      ];
-      shellHook = ''
-        test ~/.zshrc && exec zsh
-      '';
-    };
-    packages = rec {
-      lunarfetch = naersk.lib.${system}.buildPackage {
-        pname = "lunarfetch";
-        root = ./.;
-        nativeBuildInputs = with pkgs; [llvmPackages_16.libcxxClang mold];
+      pkgs = import nixpkgs {
+        inherit system overlays;
       };
+    in
+      with pkgs; {
+        formatter = pkgs.alejandra;
+        devShells.default = mkShell {
+          buildInputs = with pkgs; [
+            pkgs.zsh
+            pkgs.nil
+            rust-bin.beta.latest.default
+            pkgs.rust-analyzer
+          ];
+          shellHook = ''
+            test ~/.zshrc && exec zsh
+          '';
+        };
+        packages = rec {
+          lunarfetch = naersk.lib.${system}.buildPackage {
+            pname = "lunarfetch";
+            root = ./.;
+            nativeBuildInputs = with pkgs; [llvmPackages_16.libcxxClang mold];
+          };
 
-      default = lunarfetch;
-    };
+          default = lunarfetch;
+        };
 
-    apps ={
-      lunarfetch = {
-      type = "lunarfetch";
-      program = self.packages.lunarfetch;
-      };
-      defaultApp = lunarfetch;
-    };
-  });
+        apps = {
+          lunarfetch = {
+            type = "lunarfetch";
+            program = self.packages.lunarfetch;
+          };
+          defaultApp = lunarfetch;
+        };
+      });
 }
