974673ba3f0d570fb47556e60b4ab66fcbf2c31f
Luna <luna.dragon@suse.com>
2023-10-16 18:25:28 +0530

    add flake with nix run and package for nixos box

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "A very basic for development and packaging of lunarfetch";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nil = {
+      url = "github:oxalica/nil";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+     naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+     };
+  };
+
+  outputs = { self, nixpkgs, nil, rust-overlay,flake-utils, naersk  }: 
+  flake-utils.lib.eachDefaultSystem (system:
+    let 
+      overlays = [(import rust-overlay)];
+    pkgs = import nixpkgs {
+      inherit system overlays;
+    }; in with pkgs; {
+    formatter = pkgs.alejandra;
+    devShells.default = mkShell {
+      buildInputs = with pkgs; [
+        pkgs.zsh
+        pkgs.nil
+        rust-bin.beta.latest.default
+        pkgs.rust-analyzer
+      ];
+      shellHook = ''
+        test ~/.zshrc && exec zsh
+      '';
+    };
+    packages = rec {
+      lunarfetch = naersk.lib.${system}.buildPackage {
+        pname = "lunarfetch";
+        root = ./.;
+        nativeBuildInputs = with pkgs; [llvmPackages_16.libcxxClang mold];
+      };
+
+      default = lunarfetch;
+    };
+
+    apps ={
+      lunarfetch = {
+      type = "lunarfetch";
+      program = self.packages.lunarfetch;
+      };
+      defaultApp = lunarfetch;
+    };
+  });
+}
--- src/utils/package_managers.rs
+++ src/utils/package_managers.rs
@@ -1,12 +1,13 @@
 use super::which::which;
 use anyhow::Result;
-use std::{fs::read_dir, process::Command};
+use std::{env::var, fs::read_dir, path::PathBuf, process::Command};
 
 pub fn get_package_number() -> Result<String> {
     let mut rpm_number = 0;
     let mut flatpak_number = 0;
     let mut emerge_number = 0;
     let mut brew = 0;
+    let mut nix = 0;
     let mut out = String::from("(");
     if which("rpm").is_some() {
         let rpm_out = Command::new("rpm").arg("-qa").output()?;
@@ -67,6 +68,57 @@ pub fn get_package_number() -> Result<String> {
         }
     }
 
+    if which("nix-store").is_some() {
+        if PathBuf::from("/run/current-system/sw").exists() {
+            let nix_system_out = Command::new("nix-store")
+                .args(["-q", "--requisites", "/run/current-system/sw"])
+                .output()?;
+            let output = String::from_utf8_lossy(&nix_system_out.stdout);
+            nix = nix + output.lines().count();
+            if nix > 0 {
+                if out == "(" {
+                    out.push_str("nix-system");
+                } else {
+                    out.push_str(", nix-system");
+                }
+            }
+        }
+
+        if PathBuf::from(format!("{}/.nix-profile", var("HOME")?)).exists() {
+            let nix_user_out = Command::new("nix-store")
+                .args([
+                    "-q",
+                    "--requisites",
+                    &format!("{}/.nix-profile", var("HOME")?),
+                ])
+                .output()?;
+            let output = String::from_utf8_lossy(&nix_user_out.stdout);
+            nix = nix + output.lines().count();
+            if nix > 0 {
+                if out == "(" {
+                    out.push_str("nix-user");
+                } else {
+                    out.push_str(", nix-user");
+                }
+            }
+        }
+
+        if PathBuf::from("/nix/var/nix/profiles/default").exists() {
+            let nix_default_out = Command::new("nix-store")
+                .args(["-q", "--requisites", "/nix/var/nix/profiles/default"])
+                .output()?;
+            let output = String::from_utf8_lossy(&nix_default_out.stdout);
+            nix = nix + output.lines().count();
+            if nix > 0 {
+                if out == "(" {
+                    out.push_str("nix-defualt");
+                } else {
+                    out.push_str(", nix-defualt");
+                }
+            }
+        }
+    }
+
     if which("flatpak").is_some() {
         let flatpak_out = Command::new("flatpak").arg("list").output()?;
         let output = String::from_utf8_lossy(&flatpak_out.stdout);
@@ -84,7 +136,7 @@ pub fn get_package_number() -> Result<String> {
 
     Ok(format!(
         "{} {}",
-        emerge_number + rpm_number + flatpak_number + brew,
+        emerge_number + rpm_number + nix + flatpak_number + brew,
         out.trim()
     ))
 }
